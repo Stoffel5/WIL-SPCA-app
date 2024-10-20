64107163dbe322c6fc42765b81d56d233bcab1e5
garthdp <garthdp4@gmail.com>
2024-10-20 14:20:46 +0200

    added firebase

--- WIL-SPCS/Program.cs
+++ WIL-SPCS/Program.cs
@@ -1,5 +1,8 @@
+using Google.Cloud.Firestore;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Google.Api;
 
 namespace WIL_SPCS
 {
@@ -7,6 +10,10 @@ namespace WIL_SPCS
     {
         public static async Task Main(string[] args)
         {
+
+            string path = @"C:\Users\User\source\repos\WIL-SPCA-app\WIL-SPCS\wilspca-firebase-adminsdk-vhocd-efcb1bb217.json"; ;
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
