c4d1b4088abcc5ed11b1e3da187d6b556dbbdf26
Kimberly Ngwenya <ST10132622@vcconnect.edu.za>
2024-10-28 03:38:45 +0200

    created new side bar

---
+++ WIL-SPCS/SPCA_Navigation/script.js
@@ -0,0 +1,9 @@
+﻿const sidebarToggle = document.querySelector('.sidebar');
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 768) {
+        sidebarToggle.classList.add('collapsed');
+    } else {
+        sidebarToggle.classList.remove('collapsed');
+    }
+});
